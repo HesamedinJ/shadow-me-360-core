b95a12f86e5a6a61cb156223731e08b7cb639397
HesamedinJ <92620716+HesamedinJ@users.noreply.github.com>
2025-07-10 11:14:25 +0330

    💾 Add simple localStorage handler

--- core/src/data/storage.js
+++ core/src/data/storage.js
@@ -1,1 +1,8 @@
-js\nexport const loadData = () => {/* TODO */};\n
+export const saveData = (key, value) => {
+  localStorage.setItem(key, JSON.stringify(value));
+};
+
+export const loadData = (key) => {
+  const item = localStorage.getItem(key);
+  return item ? JSON.parse(item) : null;
+};
